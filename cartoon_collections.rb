207a508d84dae406735bef00bb7f2ec0c6b952ff
Larry Hawelu <lhawelu@gmail.com>
2021-04-06 16:01:41 -0700

    commit tree 8c728c44e7b6a94ce9bb5710c9a1bf2334b4c137

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves names
+  names.each_with_index{|name, idx| puts "#{idx + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map{|call| call.capitalize() + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls short_words
+  short_words.any? {|word| word.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese snacks
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find {|snack| cheese_types.include?(snack)}
 end
